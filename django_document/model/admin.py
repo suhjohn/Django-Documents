5281ff351fa2000cb3c9f4c2286ae7dc8ee4f595
John Sangwon Suh <johnsuh94@gmail.com>
2017-10-10 15:30:53 +0900

    added Twitter User model

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Person, Manufacturer, Student, Car, Fruit
+from .models import Person, Manufacturer, Student, Car, Fruit, Topping, Pizza, TwitterUser
 
 admin.site.register(Person)
 admin.site.register(Manufacturer)
 admin.site.register(Student)
 admin.site.register(Car)
 admin.site.register(Fruit)
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(TwitterUser)
--- django_document/model/models/many_to_many.py
+++ django_document/model/models/many_to_many.py
@@ -3,12 +3,15 @@ from django.db import models
 __all__ = (
     'Pizza',
     'Topping',
+    'TwitterUser',
 )
+
 
 class Pizza(models.Model):
     name = models.CharField(max_length=30)
     topping = models.ManyToManyField(
         'Topping',
+        blank=True,
     )
 
     def __str__(self):
@@ -18,6 +21,15 @@ class Pizza(models.Model):
 class Topping(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
 
+class TwitterUser(models.Model):
+    name = models.CharField(max_length=50)
+    friends = models.ManyToManyField(
+        'self',
+        blank=True,
+        null=True,
+    )
     def __str__(self):
         return self.name
