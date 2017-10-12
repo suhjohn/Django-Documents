54f6fc9600799f0233edfa204585a3f47b357d3f
John Sangwon Suh <johnsuh94@gmail.com>
2017-10-12 15:37:40 +0900

    password

--- django_document/config/settings.py
+++ django_document/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
 
+    'making_queries',
     'inheritance',
     'model',
 ]
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doc',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'John',
+        'PASSWORD': '12345',
     }
 }
 
--- django_document/making_queries/admin.py
+++ django_document/making_queries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog, Author, Entry
+
+admin.site.register(Blog)
+admin.site.register(Author)
+admin.site.register(Entry)
